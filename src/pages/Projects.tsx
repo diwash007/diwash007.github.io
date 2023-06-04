65a4f8a6605dda58f3e07b5eed0df9a9ad542955
Diwash <diwashdahal75@gmail.com>
2023-06-04 21:33:41 +0545

    fix: use grid instead of flex for projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,7 +4,7 @@ import { PROJECTS } from "../data/projects";
 function Projects() {
   return (
     <div className="flex justify-center items-center w-full">
-      <div className="flex flex-wrap gap-14 justify-center items-center px-4 max-w-6xl animate-page py-nav md-py-nav">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-14 px-4 animate-page py-nav md-py-nav">
         {PROJECTS.map((project) => (
           <ProjectCard project={project} key={project.name} />
         ))}
